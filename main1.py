6e9c888d6afdce3e48c079763814fc2fef4c94e0
ompatel585 <84760165+ompatel585@users.noreply.github.com>
2023-01-08 22:37:17 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -7,6 +7,8 @@ with open('restaurant.json', 'r') as json_file:
  
 restaurant1 = json_load['restaurant1']
 restaurant2 = json_load['restaurant2']
+restaurant3 = json_load['restaurant3']
+restaurant4 = json_load['restaurant4']
 
 
 
@@ -15,11 +17,24 @@ def method_name():
 	return jsonify(restaurant1)
 
 @app.route('/')
-def index():
-    return render_template('restaurant.html',restaurant=restaurant1)
+def none():
+    return "Hello World"
+
 @app.route('/res1')
 def res1():
+    return render_template('restaurant.html',restaurant=restaurant1)
+
+@app.route('/res2')
+def res2():
     return render_template('restaurant.html',restaurant=restaurant2)
+
+@app.route('/res3')
+def res3():
+    return render_template('restaurant.html',restaurant=restaurant3)
+
+@app.route('/res4')
+def res4():
+    return render_template('restaurant.html',restaurant=restaurant4)
 
 if __name__ == '__main__':
     app.run(debug=True)
